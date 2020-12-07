3e1379b8d6d1bbe907b11084deb4f0aab4fcb35e
Christian Mills <millsy4@hotmail.com>
2020-12-07 03:46:56 -0600

    have the ability to find managers now and separated some functions

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ function start() {
     "Add Department",
     "Update Employee Role"]
   }).then(function({initialize}) {
-    console.log(initialize);
     if(initialize === "View All Employees") {
       let query = `SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
       FROM employee
@@ -53,92 +52,16 @@ function start() {
         start();
       });
     } else if(initialize === "View All Departments") {
-      let query = `SELECT name AS department FROM department`;
-      connection.query(query, function(err, res) {
-        if(err) throw err;
-        console.table(res);
-        start();
-      });
+      viewAllDepartments();
     } else if(initialize === "View All Employees By Department") {
-      let query = `SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
-      FROM employee
-      LEFT JOIN role ON employee.role_id=role.id
-      LEFT JOIN department ON department_id=department.id
-      LEFT JOIN employee AS manager ON employee.manager_id=manager.id
-      ORDER BY department.name;`;
-      connection.query(query, function(err, res) {
-        if(err) throw err;
-        console.table(res);
-        start();
-      });
-
+      viewEmployeesByDepartment();
     } else if(initialize === "View All Employees By Manager") {
-      let query = `SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
-      FROM employee
-      LEFT JOIN role ON employee.role_id=role.id
-      LEFT JOIN department ON department_id=department.id
-      LEFT JOIN employee AS manager ON employee.manager_id=manager.id
-      ORDER BY manager.id;`;
-      connection.query(query, function(err, res) {
-        if(err) throw err;
-        console.table(res);
-        start();
-      });
+      viewEmployeesByManager();
     } else if(initialize === "Add Employee") {
-      let roles = [];
-      let managers = [];
-      let query = `SELECT title, salary, department.name as department
-      FROM role
-      LEFT JOIN department ON role.department_id=department.id`
-      connection.query(query, function(err, res) {
-        if(err) throw err;
-        res.forEach(role => {
-          roles.push(role.title);
-        });
-      query = `SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
-      FROM employee
-      LEFT JOIN role ON employee.role_id=role.id
-      LEFT JOIN department ON department_id=department.id
-      LEFT JOIN employee AS manager ON employee.manager_id=manager.id
-      ORDER BY manager.id;`;
-      connection.query(query, function(err, res) {
-        if(err) throw err;
-        console.log(res)
-      });
-
-        inquirer.prompt([
-        {
-          name: "firstName",
-          type: "input",
-          message: "What is the first name of the employee?",
-        },
-        {
-          name: "lastName",
-          type: "input",
-          message: "What is the last name of the employee?",
-        },
-        {
-          name: "role",
-          type: "list",
-          message: "What is the role of the employee?",
-          choices: roles
-        },
-        {
-          name: "manager",
-          type: "list",
-          message: "Who is their manager?",
-          choices: managers
-        }
-        ]).then(function( {firstName, lastName, role} ) {
-          console.log(firstName);
-          console.log(lastName);
-          console.log(role);
-        });
-        // query = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-        // VALUES (?, ?, ?, ?)`;
-        // connection.query(query, )
-      });
-
+      addEmployee(getEmployeeRoles(), getManagers());
+      // query = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+      // VALUES (?, ?, ?, ?)`;
+      // connection.query(query, )
     } else if(initialize === "Add Role") {
 
     } else if(initialize === "Add Department") {
@@ -146,15 +69,117 @@ function start() {
     } else if(initialize === "Update Employee Role") {
 
     }
-    // else if(initialize === "Remove Employee") {
-    // }
-    // else if(initialize === "Remove Department") {
-    // }
-    // else if(initialize === "Remove Role") {
-    // }
-    // else if(initialize === "Update Employee Manager") {
-    // }
   });
 };
 
-module.exports = connection;
+function viewAllDepartments() {
+  let query = `SELECT name AS department FROM department`;
+  connection.query(query, function(err, res) {
+    if(err) throw err;
+    console.table(res);
+    start();
+  });
+}
+
+function viewEmployeesByDepartment() {
+  let query = `SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN role ON employee.role_id=role.id
+  LEFT JOIN department ON department_id=department.id
+  LEFT JOIN employee AS manager ON employee.manager_id=manager.id
+  ORDER BY department.name;`;
+  connection.query(query, function(err, res) {
+    if(err) throw err;
+    console.table(res);
+    start();
+  });
+}
+
+function viewEmployeesByManager() {
+  let query = `SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN role ON employee.role_id=role.id
+  LEFT JOIN department ON department_id=department.id
+  LEFT JOIN employee AS manager ON employee.manager_id=manager.id
+  ORDER BY manager.id;`;
+  connection.query(query, function(err, res) {
+    if(err) throw err;
+    console.table(res);
+    start();
+  });
+}
+
+function addEmployee(roles, managers) {
+  inquirer.prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "What is the first name of the employee?",
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "What is the last name of the employee?",
+    },
+    {
+      name: "role",
+      type: "list",
+      message: "What is the role of the employee?",
+      choices: roles
+    },
+    {
+      name: "manager",
+      type: "list",
+      message: "Who is their manager?",
+      choices: managers
+    }]).then(function({firstName, lastName, role, manager}) {
+      console.log(firstName);
+      console.log(lastName);
+      console.log(role);
+      console.log(manager);
+    });
+}
+
+function getEmployeeRoles() {
+  let query = `SELECT title, salary, department.name as department
+  FROM role
+  LEFT JOIN department ON role.department_id=department.id`;
+  let roles = [];
+  connection.query(query, function(err, res) {
+    if(err) throw err;
+    res.forEach(role => {
+      roles.push(role.title);
+    });
+  });
+  return roles;
+}
+
+function getManagers() {
+  let query = `SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN role ON employee.role_id=role.id
+  LEFT JOIN department ON department_id=department.id
+  LEFT JOIN employee AS manager ON employee.manager_id=manager.id
+  ORDER BY manager.id;`;
+  let managers = ['None'];
+  let filteredManagers = [];
+  connection.query(query, function(err, res) {
+    if(err) throw err;
+    res.forEach(employee => {
+      if(employee.manager != null) {
+        managers.push(employee.manager);
+      }
+    });
+    managers.forEach(manager => {
+      if(filteredManagers.indexOf(manager) < 0) {
+        filteredManagers.push(manager);
+      };
+    })
+  });
+  return filteredManagers;
+}
+
+
+// function addEmployeeQuestions {
+
+// }
