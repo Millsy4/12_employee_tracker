4547d538ea2622b60e597012b095f9f8a9031339
Christian Mills <millsy4@hotmail.com>
2020-12-06 22:36:30 -0600

    getting all of the view questions working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+// const employeeQueries = require("./employeeQueries");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -20,21 +21,39 @@ function start() {
     type: "list",
     message: "What would you like to do?",
     choices: ["View All Employees", 
+    "View All Roles",
+    "View All Departments",
     "View All Employees By Department", 
     "View All Employees By Manager", 
     "Add Employee", 
-    "Remove Employee",
     "Add Role",
-    "Remove Role", 
-    "Update Employee Role", 
-    "Update Employee Manager"]
+    "Add Department",
+    "Update Employee Role"]
   }).then(function({initialize}) {
     console.log(initialize);
     if(initialize === "View All Employees") {
-      
+      const info = connection.query(`SELECT employee.id, employee.first_name AS first, employee.last_name AS last, role.title, department.name, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
+      FROM employee
+      LEFT JOIN role ON employee.role_id=role.id
+      LEFT JOIN department ON department_id=department.id
+      LEFT JOIN employee AS manager ON employee.manager_id=manager.id
+      ORDER BY employee.id;`, function(err, res){
+        if(err) throw err;
+        console.table(res);
+        start();
+      });
     } else if(initialize === "View All Roles") {
-
+      const info = connection.query(`SELECT title, salary, department.name as department
+      FROM role
+      LEFT JOIN department ON role.department_id=department.id`, function(err, res) {
+        if(err) throw err;
+        console.table(res);
+      });
     } else if(initialize === "View All Departments") {
+      const info = connection.query(`SELECT name AS department FROM department`, function(err, res) {
+        if(err) throw err;
+        console.table(res);
+      });
   
     } else if(initialize === "View All Employees By Department") {
 
@@ -59,3 +78,5 @@ function start() {
     // }
   });
 };
+
+module.exports = connection;
