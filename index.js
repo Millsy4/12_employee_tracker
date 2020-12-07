5d08b78adaa3fff39ff51bc0b4b4550b8b3ee851
Christian Mills <millsy4@hotmail.com>
2020-12-06 18:12:10 -0600

    added gitignore

---
+++ index.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Chieff1!",
+  database: "employee_db"
+});
+
+connection.connect(function(err) {
+  if(err) throw err;
+  start();
+});
+
+function start() {
+  inquirer.prompt({
+    name: "initialize",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+  }).then(function(res) {
+    console.log(res);
+  });
+};
