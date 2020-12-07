f738dd5a1be6503cd6f54201ee367c937dae0c8d
Christian Mills <millsy4@hotmail.com>
2020-12-06 18:21:53 -0600

    updated question fields after receiving initial request

--- index.js
+++ index.js
@@ -18,8 +18,29 @@ function start() {
     name: "initialize",
     type: "list",
     message: "What would you like to do?",
-    choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
-  }).then(function(res) {
-    console.log(res);
+    choices: ["View All Employees", 
+    "View All Employees By Department", 
+    "View All Employees By Manager", 
+    "Add Employee", 
+    "Remove Employee", 
+    "Update Employee Role", 
+    "Update Employee Manager"]
+  }).then(function({initialize}) {
+    console.log(initialize);
+    if(initialize === "View All Employees") {
+      
+    } else if(initialize === "View All Employees By Department") {
+
+    } else if(initialize === "View All Employees By Manager") {
+
+    } else if(initialize === "Add Employee") {
+
+    } else if(initialize === "Remove Employee") {
+
+    } else if(initialize === "Update Employee Role") {
+
+    } else if(initialize === "Update Employee Manager") {
+      
+    }
   });
 };
